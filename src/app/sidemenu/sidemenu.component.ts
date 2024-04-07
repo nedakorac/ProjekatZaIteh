e4399c4d7d63b73588df2951c9ea7e328100b2ac
nedakorac <koraciii00@gmail.com>
2024-04-07 16:38:07 +0200

    Dodat sidemnu

--- src/app/sidemenu/sidemenu.component.ts
+++ src/app/sidemenu/sidemenu.component.ts
@@ -1,21 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
   styleUrl: './sidemenu.component.css'
 })
-export class SidemenuComponent {
+export class SidemenuComponent implements OnInit{
 
   filter1: boolean = false;
   filter2: boolean = false;
   filter3: boolean = false;
 
-  constructor() { }
+  public selectedPage: string = "";
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter((event): event is NavigationEnd => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.selectedPage = event.url;
+      console.log(this.selectedPage);
+    });
+  }
+
 
   selectPrice(price: string) {
     console.log("Selected price:", price);
-    // Implement your logic here
   }
 
 }
