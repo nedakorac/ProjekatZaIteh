5a8d0606e1b74409195ca109038b4471a79eca7d
makljenovicana <makljenovicana@gmail.com>
2024-04-25 21:32:07 +0200

    dodata logika za login i register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,28 +1,37 @@
 import { Component } from '@angular/core';
+import { first } from 'rxjs';
+import { User } from '../models/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-  user = { email: '', password: '' };
+  constructor(private userService: UserService) {}
 
-  login(email: string, password: string) {
-    this.user.email = email;
-    this.user.password = password;
-    // Ovde dodajte logiku za prijavljivanje korisnika, na primer poziv API-ja
-    console.log('User logged in', this.user);
+  login(loginEmail: HTMLInputElement, loginPassword: HTMLInputElement) {
+    const email = loginEmail.value;
+    const password = loginPassword.value;
+    
+    let succesfullyLoggedIn = this.userService.login(email, password);
+
+    if(succesfullyLoggedIn){
+      alert("Uspesno ste se ulogovali!");
+    }
+    else{
+      alert("Neuspesno logovanje");
+    }
   }
-  register(firstName: string, lastName: string, email: string, password: string) {
-  /*  this.user = {
-      firstName,
-      lastName,
-      email,
-      password,
-    };
-    // Ovde dodajte logiku za registraciju korisnika, na primer poziv API-ja
-    console.log('User registered', this.user);*/
+
+
+  register(firstName: HTMLInputElement, lastName: HTMLInputElement, registerEmail: HTMLInputElement, registerPassword: HTMLInputElement) {
+    let fullName = firstName.value + " " + lastName.value;
+    let user = new User(0, fullName, registerEmail.value, registerPassword.value, undefined )
+   
+    this.userService.register(user);
+    
   }
 }
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { BehaviorSubject, Observable, catchError, map, of, throwError } from 'rxjs';
+import { Token } from '@angular/compiler';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private loggedInUser = new BehaviorSubject<User>(new User(0, "", "",""));
+  public user$ = this.loggedInUser.asObservable();
+
+  constructor(private httpClient: HttpClient) { }
+
+  register(user: User): void {
+    let userData = { name: user.name, email: user.email, password: user.password };
+
+      this.httpClient.post<{user: User, token: string}>('http://127.0.0.1:8000/api/register', userData).subscribe(user =>{
+      let newUser = new User(user.user.id, user.user.name, user.user.email, undefined, user.token);
+      this.loggedInUser.next(newUser);
+      console.log(newUser);
+    });
+  }
+
+  login(email: string, password: string): Observable<boolean> {
+    const body = { email, password };
+
+    return this.httpClient.post<{user: User, token: string}>('http://127.0.0.1:8000/api/login', body)
+      .pipe(
+        map(user => {
+          let newUser = new User(user.user.id, user.user.name, user.user.email, undefined, user.token);
+          return true;
+        }), 
+        catchError(error => of(false)) 
+      );
+  }
+
+}
